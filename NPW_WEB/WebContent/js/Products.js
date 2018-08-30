1c160a6a32578a268f1761e461681b38f237a19e
amit <malpureamit@gmail.com>
2018-08-30 14:27:48 +1000

    url parameters

---
+++ NPW_WEB/WebContent/js/Products.js
@@ -0,0 +1,42 @@
+	var products = [
+		{
+			productID: 'P1',
+			productName: 'productOne',
+			productDesc: 'Product One description here Lorem ipsum dolor sit amit, consectetur adipiscing elit - amit2',
+			rate: 100,
+			minQty: 1,
+			maxQty: 5
+		},
+		{
+			productID: 'P2',
+			productName: 'productTwo',
+			productDesc: 'Product Two description here Lorem ipsum dolor sit amit, consectetur adipiscing elit',
+			rate: 50,
+			minQty: 2,
+			maxQty: 10
+		},
+		{
+			productID: 'P3',
+			productName: 'productThree',
+			productDesc: 'Product Three description here Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			rate: 10,
+			minQty: 1,
+			maxQty: 1
+		},
+		{
+			productID: 'P4',
+			productName: 'productFour',
+			productDesc: 'Product Four description here Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			rate: 10,
+			minQty: 1,
+			maxQty: 10
+		},
+		{
+			productID: 'P5',
+			productName: 'productFive',
+			productDesc: 'Product Five description here Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			rate: 43,
+			minQty: 43,
+			maxQty: 43
+		}
+	];
