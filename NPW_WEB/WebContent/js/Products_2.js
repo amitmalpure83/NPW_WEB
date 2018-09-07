b1bd2796836338bf98cc45469be26a2b82c47053
amit <malpureamit@gmail.com>
2018-09-07 17:14:03 +1000

    fixes

---
+++ NPW_WEB/WebContent/js/Products_2.js
@@ -0,0 +1,68 @@
+	var products = [
+		{
+			Product_ID: 	'NPW1001',
+			Name: 			'NPW1011: RED BLISS',
+			Desc: 			'This designer name plate, exclusively handcrafted for you, will help in creating divine impression for your home entrance.',
+			Type:			'Door Nameplates',
+			Sub_Type:		'Wooden Nameplates',
+			Customiation_1:	'Idol,Golden Ganesh,0',
+			Customiation_2:	'No_Of_Lines,2,0',
+			Dimensions: 	'15*45*4CM',
+			Customizable: 	'Y',
+			Feature_1: 		'Hanging:Wall mountable with 2 screw hooks',				
+			Feature_2: 		'Make:Handcrafted',				
+			Feature_3: 		'Material:Art Wooden Board',				
+			Feature_4: 		'Letter Color:Golden',			
+			Feature_5:		'Board Color:Red,Green',
+			Price:			'140',
+			Offer:			'40',
+			Available_Count:'2',
+			Sold_Count:		'0',	
+			Review_Code:	'0'
+		},
+		{
+			Product_ID: 	'NPW1002',
+			Name: 			'NPW1011: RED BLISS',
+			Desc: 			'This designer name plate, exclusively handcrafted for you, will help in creating divine impression for your home entrance.',
+			Type:			'Door Nameplates',
+			Sub_Type:		'Wooden Nameplates',
+			Customiation_1:	'Idol,Golden Ganesh,0',
+			Customiation_2:	'No_Of_Lines,2,0',
+			Dimensions: 	'15*45*4CM',
+			Customizable: 	'Y',
+			Feature_1: 		'Hanging:Wall mountable with 2 screw hooks',				
+			Feature_2: 		'Make:Handcrafted',				
+			Feature_3: 		'Material:Art Wooden Board',				
+			Feature_4: 		'Letter Color:Golden',			
+			Feature_5:		'Board Color:Red,Green',
+			Price:			'140',
+			Offer:			'40',
+			Available_Count:'2',
+			Sold_Count:		'0',	
+			Review_Code:	'0'
+		},
+		{
+			productID: 'P3',
+			productName: 'productThree',
+			productDesc: 'Product Three description here Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			rate: 10,
+			minQty: 1,
+			maxQty: 1
+		},
+		{
+			productID: 'P4',
+			productName: 'productFour',
+			productDesc: 'Product Four description here Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			rate: 10,
+			minQty: 1,
+			maxQty: 10
+		},
+		{
+			productID: 'P5',
+			productName: 'productFive',
+			productDesc: 'Product Five description here Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			rate: 43,
+			minQty: 43,
+			maxQty: 43
+		}
+	];
