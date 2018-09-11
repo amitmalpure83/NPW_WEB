6cfb1b1480e6449591237e0f5bb9140c2c95fc6b
amit <malpureamit@gmail.com>
2018-09-11 13:59:14 +1000

    fixes

---
+++ NPW_WEB/WebContent/js/Cart_20.js
@@ -0,0 +1,73 @@
+$( document ).ready(function() {
+	
+	var sParam = 'Product_ID';
+	
+	var sPageURL = window.location.search.substring(1);
+
+    var url = window.location.href;
+
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+        	Product_ID = sParameterName[1];
+        }
+    }
+    
+	for(var i = 0; i < products.length; i++) 
+	{
+		if(products[i].productID == Product_ID)
+		{
+			$("#productsDiv").append(
+				"<div class='flex-container'>"
+					+"<div>"+ products[i].productID +" - "+ products[i].productName +"</div>"
+					+"<div>"+ products[i].productDesc  +"</div>"
+					+"<div> Rate: "+ products[i].rate +"</div>"
+					+"<div> Quantity: <input type='text' id='"+ products[i].productID +"' name='"+ products[i].productID +"'></div>"
+				+"</div>"
+			);
+			
+			document.getElementById("Product_Name").innerHTML = products[i].productName;
+			document.getElementById("Product_Price").innerHTML = products[i].rate;
+		}
+	}
+		
+	
+	for(var i = 0; i < products5.length; i++) 
+	{
+		if(products5[i].Product_ID == Product_ID)
+		{
+			document.getElementById("Product_Name").innerHTML = "Amit5";
+		}
+	}
+	
+	$("#productsDiv").append("<input type='submit'>");
+		
+	});
+
+
+function GetURLParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+
+    var url = window.location.href;
+
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+function setURLParameter() {
+    var Product_Name = GetURLParameter('Product_ID');
+    document.getElementById("Product_ID").innerHTML = Product_Name;
+
+    var Product_Name = GetURLParameter('Product_ID2');
+    document.getElementById("Product_ID2").innerHTML = Product_Name;
+}
