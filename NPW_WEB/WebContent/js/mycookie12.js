5b1732a99c1038143900f041027027362feb27be
amit <malpureamit@gmail.com>
2018-09-20 22:53:15 +1000

    updates

--- NPW_WEB/WebContent/js/mycookie12.js
+++ NPW_WEB/WebContent/js/mycookie12.js
@@ -59,8 +59,16 @@ function populateCartInHome()
 	{
 		var Product1CKArr = Product1CK.split(',');
 	
+		var totalCartCountStr = document.getElementById("cartCount").innerHTML;
+		var totalCardAmoutStr = document.getElementById("cartPrice").innerHTML;
+		totalCardAmoutStr = totalCardAmoutStr.replace("$", "");
+		totalCardAmoutStr = totalCardAmoutStr.replace("&nbsp;", "");
+		
 		var cartItemStr = "";
-	
+		
+		var totalCardAmout = parseInt(totalCardAmoutStr);
+		var totalCartCount = parseInt(totalCartCountStr);
+		
 		for(i = 0; i < Product1CKArr.length; i++)
 		{
 			var productDetails = Product1CKArr[i];
@@ -69,12 +77,24 @@ function populateCartInHome()
 			var productName = productDetailsArr[1];
 			var productPrice = productDetailsArr[2];
 	
+			totalCartCount++;
+			totalCardAmout = totalCardAmout + parseInt(productPrice);
+			
 			cartItemStr = 
 				"<div class=\"dropdown-product-item\"><span class=\"dropdown-product-remove\"><i class=\"icon-cross\"></i></span><a class=\"dropdown-product-thumb\" " +
 				"href=\"shop-single.html?Product_ID=" + productId + "\"><img src=\"img/shop/products/nameplates/" + productId + "_th01.jpg\" alt=\"Product\"></a>"+
 			    "<div class=\"dropdown-product-info\"><a class=\"dropdown-product-title\" href=\"shop-single.html?Product_ID=" + productId + "\">" + productName + "</a><span class=\"dropdown-product-details\">1 x $" + productPrice + "</span></div> </div>";	
 		}
 		
+		var cartPriceStr = "<div class=\"toolbar-dropdown-group\"><div class=\"column\"><span class=\"text-lg\">Total:</span></div>"+
+		"<div class=\"column text-right\"><span class=\"text-lg text-medium\">$" + totalCardAmout + "&nbsp;</span></div></div><div class=\"toolbar-dropdown-group\">"+
+		"<div class=\"column\"><a class=\"btn btn-sm btn-block btn-secondary\" href=\"cart.html\">View Cart</a></div>"+
+		"<div class=\"column\"><a class=\"btn btn-sm btn-block btn-success\" href=\"checkout-address.html\">Checkout</a></div></div>";  
+			
+			
 		document.getElementById("cartIndex").innerHTML = document.getElementById("cartIndex").innerHTML + cartItemStr;
+		document.getElementById("cartPrice").innerHTML = "$" + totalCardAmout + "&nbsp;";
+		document.getElementById("cartPrice1").innerHTML = "$" + totalCardAmout;
+		document.getElementById("cartCount").innerHTML = totalCartCount;
 	}
 }
